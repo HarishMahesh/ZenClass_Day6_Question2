414f913468506eaddb2656a8482db9a07db04d52
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-04 23:05:06 +0530

    Add files via upload

---
+++ subquestion3/script.js
@@ -0,0 +1,10 @@
+alert("I'm JavaScript!");
+alert('Hello') // this line is not having semicolon  --> it soptional
+alert(`Wor
+ ld`)     // wor will be displayed in first line and ld in next line
+alert(3 +
+1
++ 2); // this is multiple line code and its working  ---. yes it will work it will add all three numbers and show it as alert
+
+
+// once one alert is accepted continious the subsequent alerts will be coming.
