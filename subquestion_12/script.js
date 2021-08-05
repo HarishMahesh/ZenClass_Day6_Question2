e57a00a16cdf9d9208ba8cad7d20f5548bc0c5ba
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-05 22:43:36 +0530

    Add files via upload

---
+++ subquestion_12/script.js
@@ -0,0 +1,12 @@
+let message;
+let lock = 2;
+//Dont change any code below this 
+if (null && lock && undefined )   // changing or operator to and
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+  console.log(message);
