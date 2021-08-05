2036ee5e106f97d28a85d1638284708262f538fa
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-05 22:39:51 +0530

    Add files via upload

---
+++ subquestion_11/script.js
@@ -0,0 +1,11 @@
+// You cant change the value of the msg
+let message;
+if (null || 2 || undefined )
+{
+ message = "welcome boss";   // already it is declared outside so no need to declare it again
+}
+else
+{
+ message = "Go away";   // already it is declared outside so no need to declare it again
+}
+  console.log(message);
