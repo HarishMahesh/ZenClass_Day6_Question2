d7759939ba49f1700e7f1db0ffca3a23e3438761
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-05 22:49:17 +0530

    Add files via upload

---
+++ subquestion_13/script.js
@@ -0,0 +1,12 @@
+let message;
+let lock = 2;
+//Dont change any code below this
+if (lock && "" || undefined) // " " there was space so it is considered as a values so it is changed to ""
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
