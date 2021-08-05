f15cead094a264f9e742f2df4ed0d200bc10e8eb
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-05 22:19:24 +0530

    Add files via upload

---
+++ subquestion7/script.js
@@ -0,0 +1,11 @@
+var a = "2" > "12";  // when both are strings it will check alphabetically so 2 comes after 1 so it is true.
+
+a = 2 > '12'  // to get diffused it should be changed lyk this
+//Don't touch below this
+if (a) {
+  console.log("Code is Blasted")
+}
+else
+{
+  console.log("Diffused") 
+}
