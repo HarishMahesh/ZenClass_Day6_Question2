c46a8edfbd2b79d28cd0bde02cb556db149592ba
HarishMahesh <87910362+HarishMahesh@users.noreply.github.com>
2021-08-05 22:28:17 +0530

    Add files via upload

---
+++ subquestion9/script.js
@@ -0,0 +1,8 @@
+let value = +prompt('How many runs you scored in this ball');  // string input is converted to number
+if (value === 4) {
+      console.log("You hit a Four");
+} else if (value === 6) {
+      console.log("You hit a Six");
+} else {
+      console.log("I couldn't figure out");
+}
